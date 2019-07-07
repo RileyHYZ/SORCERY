1a73e5d90d9db13b32a4ee7d0d5385863df3660a
Michelle <zheng.michelle.99@gmail.com>
2019-07-07 14:10:26 -0400

    Add Color field to Square

--- square.cc
+++ square.cc
@@ -1,5 +1,9 @@
 #include <memory>
+
 #include "square.h"
+#include "chessPiece.h"
+#include "card.h"
+#include "color.h"
 
 using namespace std;
 
@@ -15,8 +19,12 @@ Card Square::getCard() {
     return card;
 }
 
+Color Square::getColor() {
+    return color;
+}
+
 // Constructor
 
 // this will change depending on how you want to initialize the board, if 
 // initialize all at once or initialize squares first, then put pieces/cards in
-Square::Square(ChessPiece* p, Card c) : piece{p}, card{c} {}
+Square::Square(ChessPiece* p, Card c, Color co) : piece{p}, card{c}, color{co} {}
--- square.h
+++ square.h
@@ -2,17 +2,21 @@
 #define __SQUARE_H__
 
 #include <memory>
+
 #include "chessPiece.h"
 #include "card.h"
+#include "color.h"
 
 class Square {
     ChessPiece* piece;
     Card card;
+    Color color;
 
   public:
-    Square(ChessPiece*, Card);
+    Square(ChessPiece*, Card, Color);
     ChessPiece* getPiece();
     Card getCard();
+    Color getColor();
 };
 
 #endif
