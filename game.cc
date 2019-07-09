4e1b30efb7c8f87972752860b71983cc395b5941
RileyHYZ <ri.hyzhou@gmail.com>
2019-07-09 01:28:22 -0400

    Add checkWin function

--- game.cc
+++ game.cc
@@ -8,5 +8,22 @@ using namespace std;
 Game::Game() {}
 
 bool Game::playTurn() {
+    return checkWin();
+}
 
+bool Game::checkWin() {
+    Color opponent = curPlayer == WHITE ? BLACK : WHITE;
+    if(!chessBoard.getPlayerHp(opponent) || !chessBoard.armyIsAlive(opponent)){
+        winner = curPlayer;
+        return true;
+    }
+    if(chessBoard.checkStandstill()) {
+        if (chessBoard.getPlayerHp(opponent) > chessBoard.getPlayerHp(curPlayer)){
+            winner = opponent;
+        } else {
+            winner = curPlayer;
+        }
+        return true;
+    }
+    return false;
 }
--- game.h
+++ game.h
@@ -7,6 +7,8 @@
 class Game : public Subject {
     ChessBoard chessBoard;
 		Color curPlayer;
+    Color winner;
+    bool checkWin();
   public:
     Game();
 		bool playTurn();
