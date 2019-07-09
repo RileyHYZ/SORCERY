1745a8d263dd2c67250847309b1eea9af8549ab7
RileyHYZ <ri.hyzhou@gmail.com>
2019-07-08 23:33:52 -0400

    Add piece logic

--- chessPiece.cc
+++ chessPiece.cc
@@ -8,55 +8,67 @@ ChessPiece::ChessPiece(Color c) : color{c} {}
 
 ChessPiece::~ChessPiece() {}
 
-bool ChessPiece::checkValidMove(Point& curPos, Point& newPos) {
-    return isValidMove(curPos, newPos);
+//we can add a bool to tell if the newPos has opponent's piece
+bool ChessPiece::checkValidMove(Point& curPos, Point& newPos, bool capture) {
+    return isValidMove(curPos, newPos, capture);
+}
+
+Color ChessPiece::getColor() {
+    return color;
 }
 
 // Pawn
 
 Pawn::Pawn(Color c) : ChessPiece{c} {}
 		
-bool Pawn::isValidMove(Point& curPos, Point& newPos) {
-
+bool Pawn::isValidMove(Point& curPos, Point& newPos, bool capture) {
+    if(capture) {
+        return abs(curPos.getX - newPos.getX) == 1 &&
+                    abs(curPos.getY - curPos.getY) == 1;
+    }
+    bool forwardOne = getColor() == WHITE ? newPos.getY == curPos.getY - 1 :
+                    newPos.getY == curPos.getY + 1;
+    return curPos.getX == newPos.getX && forwardOne; 
 }
-
 
 // Knight
 
 Knight::Knight(Color c) : ChessPiece{c} {}
 
-bool Knight::isValidMove(Point& curPos, Point& newPos) {
-
+bool Knight::isValidMove(Point& curPos, Point& newPos, bool cap = false) {
+    return abs(curPos.getX - newPos.getX) + abs(curPos.getY - newPos.getY) == 3;
 }
 
 // Bishop
 
 Bishop::Bishop(Color c) : ChessPiece{c} {}
 
-bool Bishop::isValidMove(Point& curPos, Point& newPos) {
-
+bool Bishop::isValidMove(Point& curPos, Point& newPos, bool cap = false) {
+    return abs(curPos.getX - newPos.getX) == abs(curPos.getY - curPos.getY);
 }
 
 // Rook
 
 Rook::Rook(Color c) : ChessPiece{c} {}
 
-bool Rook::isValidMove(Point& curPos, Point& newPos) {
-
+bool Rook::isValidMove(Point& curPos, Point& newPos, bool cap = false) {
+    return curPos.getX == newPos.getX || curPos.getY == newPos.getY;
 }
 
 // King
 
 King::King(Color c) : ChessPiece{c} {}
 
-bool King::isValidMove(Point& curPos, Point& newPos) {
-
+bool King::isValidMove(Point& curPos, Point& newPos, bool cap = false) {
+    return false;
 }
 
 // Queen
 
 Queen::Queen(Color c) : ChessPiece{c} {}
 
-bool Queen::isValidMove(Point& curPos, Point& newPos) {
-
+bool Queen::isValidMove(Point& curPos, Point& newPos, bool cap = false) {
+    return abs(curPos.getX - newPos.getX) == abs(curPos.getY - curPos.getY)
+                    || curPos.getX == newPos.getX
+                    || curPos.getY == newPos.getY;
 }
--- chessPiece.h
+++ chessPiece.h
@@ -5,48 +5,49 @@
 
 class ChessPiece {
 		Color color;
-		virtual bool isValidMove(Point&, Point&) = 0;
+		virtual bool isValidMove(Point&, Point&, bool) = 0;
 
 	public:
 		ChessPiece(Color); 
+		Color getColor();
 		virtual ~ChessPiece();
-		bool checkValidMove(Point&, Point&);
+		bool checkValidMove(Point&, Point&, bool);
 };
 
 class Pawn : public ChessPiece {
 	public:
 		Pawn(Color);
-		bool isValidMove(Point&, Point&) override;
+		bool isValidMove(Point&, Point&, bool) override;
 };
 
 class Knight : public ChessPiece {
 	public:
 		Knight(Color);
-		bool isValidMove(Point&, Point&) override;
+		bool isValidMove(Point&, Point&, bool) override;
 };
 
 class Bishop : public ChessPiece {
 	public:
 		Bishop(Color);
-		bool isValidMove(Point&, Point&) override;
+		bool isValidMove(Point&, Point&, bool) override;
 };
 
 class Rook : public ChessPiece {
 	public:
 		Rook(Color);
-		bool isValidMove(Point&, Point&) override;
+		bool isValidMove(Point&, Point&, bool) override;
 };
 
 class King : public ChessPiece {
 	public:
 		King(Color);
-		bool isValidMove(Point&, Point&) override;
+		bool isValidMove(Point&, Point&, bool) override;
 };
 
 class Queen : public ChessPiece {
 	public:
 		Queen(Color);
-		bool isValidMove(Point&, Point&) override;
+		bool isValidMove(Point&, Point&, bool) override;
 };
 
 #endif
